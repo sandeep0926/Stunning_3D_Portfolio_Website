045ab5a472157fa8ac2c4b08d037453476af572f
Andrew Tsegaye <andrewtsegaye7@gmail.com>
2024-06-15 13:02:21 +0300

    Update assests export

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../styles.js';
 import { navLinks } from '../constants';
-import { logo, menu, close } from '@assets';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, './src/assets'),
-    },
-  },
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg'],
 });
